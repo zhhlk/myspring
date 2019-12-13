05ac6c143e48c559e222443a2d52c1a287d617b0
zhang <zhang@163.com>
2019-12-13 16:13:09 +0800

    mapper ok
    Signed-off-by: zhang <zhang@163.com>

---
+++ src/main/java/com/gitdemo/po/Student.java
@@ -0,0 +1,22 @@
+package com.gitdemo.po;
+
+public class Student {
+    private String uname;
+    private String passwd;
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+    public String getPasswd() {
+        return passwd;
+    }
+
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
+    }
+}
